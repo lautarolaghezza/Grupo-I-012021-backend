9c174988920a7d4d9ddea66be213564624ede10f
llaghezza <llaghezza@veritran.com>
2021-05-04 18:21:27 -0300

    fix repository

--- src/main/java/ar/edu/unq/desapp/grupoI/backenddesappapi/BackendDesappApiApplication.java
+++ src/main/java/ar/edu/unq/desapp/grupoI/backenddesappapi/BackendDesappApiApplication.java
@@ -2,12 +2,13 @@ package ar.edu.unq.desapp.grupoi.backenddesappapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BackendDesappApiApplication {
 
     public static void main(String[] args) {
