a65741e83d36e4e3ae6f734137d66b382e839210
llaghezza <llaghezza@veritran.com>
2021-05-31 21:11:29 -0300

    se modifica el data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 INSERT INTO classifiable (dtype, tconst, title_type, primary_title, original_title, is_adult, runtime_minutes, start_year, end_year, genres) values('Title','pdc1','movie','Piratas del caribe 1','Pirates of the caribbean',false, 120 , 2010, 2011, 'action,drama');
+INSERT INTO classifiable (dtype, tconst, title_type, primary_title, original_title, is_adult, runtime_minutes, start_year, end_year, genres) values('Title','pdc2','movie','Piratas del caribe 2','Pirates of the caribbean 2',false, 120 , 2012, 2014, 'action,drama');
 
-INSERT INTO principals (category, characters, job, nconst, ordering, tconst) values ('action', 'robert de niro', 'empleado', 'rdn', 1, 'pdc1');
+INSERT INTO principals (category, characters, job, nconst, ordering, tconst) values ('action', 'johnny depp', 'capitan jack sparrow', 'rdn', 1, 'pdc1');
+INSERT INTO principals (category, characters, job, nconst, ordering, tconst) values ('action', 'johnny depp', 'pirata', 'rdn', 1, 'pdc2');
 
-INSERT INTO crew (tconst, directors, writers) values ('pdc1', 'alpacino', 'alpacino escritor');
+INSERT INTO crew (tconst, directors, writers) values ('pdc1', 'gore verbinski', 'ted elliot');
+INSERT INTO crew (tconst, directors, writers) values ('pdc2', 'gore verbinski', 'ted elliot');
