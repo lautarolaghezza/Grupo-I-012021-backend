5a582b44a6174abda5000ff0eddb77cdf4ce521e
Gustavo Barreiro <barreirogustavo05@gmail.com>
2021-07-05 21:08:53 -0300

    aplcacion corriendo

--- src/main/java/ar/edu/unq/desapp/grupoI/backenddesappapi/model/films/TitleCache.java
+++ src/main/java/ar/edu/unq/desapp/grupoI/backenddesappapi/model/films/TitleCache.java
@@ -3,10 +3,10 @@ package ar.edu.unq.desapp.grupoi.backenddesappapi.model.films;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import javax.persistence.Column;
-import javax.persistence.Id;
 
 @Getter
 @Setter
