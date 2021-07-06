b6e34c5335cc1ece506a8f9f1d417c38fc5be3b3
llaghezza <llaghezza@veritran.com>
2021-07-06 17:52:49 -0300

    se agranda un poco el coverage

--- src/test/java/ar/edu/unq/desapp/grupoI/backenddesappapi/model/films/EpisodeTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoI/backenddesappapi/model/films/EpisodeTest.java
@@ -1,5 +1,6 @@
 package ar.edu.unq.desapp.grupoi.backenddesappapi.model.films;
 
+import ar.edu.unq.desapp.grupoi.backenddesappapi.model.rating.Rating;
 import ar.edu.unq.desapp.grupoi.backenddesappapi.model.reviews.PremiumReview;
 import ar.edu.unq.desapp.grupoi.backenddesappapi.model.reviews.PublicReview;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,27 +17,25 @@ public class EpisodeTest {
     private Episode episode;
     private List<PremiumReview> critics;
     private List<PublicReview> reviews;
+    private Crew crew;
+    private Principals principals;
+    private Rating rating;
 
     @BeforeEach
     public void setUp() {
         critics = new ArrayList<>();
         reviews = new ArrayList<>();
+        crew = new Crew();
+        principals = new Principals();
+        rating = new Rating();
+        crew.setCrewtconst("crew");
+        principals.setPrincipaltconst("principals");
+        rating.setAverage_rating(4.0);
         this.episode = new Episode("friends-S1E2", "tvSeries-Friends", 1, 2, critics, reviews);
+        episode.setCrew(crew);
+        episode.setPrincipals(principals);
+        episode.setRating(rating);
     }
-
-  /*  @Test
-    public void newFriendsEpisodeOfSeason2CanGetHisRankingWithOneCriticWith5AndOtherWith4() {
-        PremiumReview premiumReview = new PremiumReview();
-        //premiumReview.setRating(5);
-        critics.add(premiumReview);
-        PublicReview publicReview = new PublicReview();
-        //publicReview.setRating(4);
-        reviews.add(publicReview);
-
-        assertEquals(4.5, this.episode.getRating());
-    }
-
-   */
 
     @Test
     public void newFriendsEpisodeHasProperties() {
@@ -49,8 +48,9 @@ public class EpisodeTest {
         assertEquals("tvSeries-Friends", episode.getParentTconst());
         assertEquals(1, episode.getSeasonNumber());
         assertEquals(2, episode.getEpisodeNumber());
-        //assertTrue(episode.getCritics().contains(premiumReview));
-        //assertTrue(episode.getReviews().contains(publicReview));
+        assertEquals(crew, episode.getCrew());
+        assertEquals(principals, episode.getPrincipals());
+        assertEquals(rating, episode.getRating());
     }
 
 }
